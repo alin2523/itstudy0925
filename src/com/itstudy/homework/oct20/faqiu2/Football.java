a2e52f5268ac78d18ac0b6ee952b53f0ef455db1
Luzifer <alin2523@gmail.com>
2021-10-26 20:00:44 +0900

    范例

---
+++ src/com/itstudy/homework/oct20/faqiu2/Football.java
@@ -0,0 +1,30 @@
+package com.itstudy.homework.oct20.faqiu2;
+
+public class Football {
+    String name;
+    int num;
+
+    public Football() {
+    }
+
+    public Football(String name, int num) {
+        this.name = name;
+        this.num = num;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+}
